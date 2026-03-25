f0024f6e0ac60116b342fd26dd4eee3caa7e8552
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2026-03-25 21:31:36 +0200

    react start

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Galvene from './Galvene'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Galvene />
+      <h1>Hello React</h1>
+      <h2>Mācos veidot WEB aplikācijas</h2>
+      <button className='btn1'>Hello</button>
+      <Galvene />
+    </>
+  )
+}
+
+export default App
---
+++ src/Galvene.jsx
@@ -0,0 +1,16 @@
+function Galvene(){
+
+    return (
+        <>
+            <header>
+                <h1>Lapas galvenais nosaukums</h1>
+                <img src="https://picsum.photos/200" alt="attēls1" />
+            </header>
+        </>
+    )
+
+
+
+}
+
+export default Galvene
